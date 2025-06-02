90aa76a94f646485339fecf3c70e1d1b5ff86849
samjackson24816 <samjackson24816@users.noreply.github.com>
2025-06-02 19:00:22 +0000

    deploy: 986ab2eb23b1ee15ca28c3fc3b3bb9acd22a2c54

--- assets/index-Dg_ZVjsO.js
+++
@@ -1,7 +0,0 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&i(c)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();function s(n){let r=0;const o=i=>{r=i,n.innerHTML=`count is ${r}`};n.addEventListener("click",()=>o(r+1)),o(0)}document.querySelector("#app").innerHTML=`
-  <div>
-    <p>Vite + TypeScript + Tailwind CSS</p>
-    <a href="https://vitejs.dev" target="_blank">
-    
-  </div>
-`;s(document.querySelector("#counter"));
